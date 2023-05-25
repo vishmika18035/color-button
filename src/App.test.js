f3b398b462d1c547fb5f1b2e6fed07112c054bc1
vishmika thilakarathne <vishmikaad@gmail.com>
2023-05-25 12:17:17 +0530

    commit

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText('Learn React');
-  echo "hii";
+const linkElement = screen.getByText('Learn React');
   expect(linkElement).toBeInTheDocument();
 });
